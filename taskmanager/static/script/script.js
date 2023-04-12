62f79b68780a6165c9749d67da7419ba8b10a9df
PJDEVEX <>
2023-04-12 22:49:09 +0000

    Add task edit functionality

--- taskmanager/static/script/script.js
+++ taskmanager/static/script/script.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 
+    //  datepicker initialization
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
       format: "mmm dd, yyyy",
       i18n: {done: "Select"}
     });
 
+    // category selection initialisation
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // Task description collapsable initialization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
